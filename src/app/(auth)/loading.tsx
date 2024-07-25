7828217b663a07f4222af0b370ff78e1a1efeb1e
cyber330d <cyber330d@gmail.com>
2024-07-25 05:34:20 +0100

    fix Routing

--- src/app/(auth)/loading.tsx
+++ src/app/(auth)/loading.tsx
@@ -1,4 +1,4 @@
 export default function Loading() {
-    // Or a custom loading skeleton component
-    return <p>Loading...</p>
+    // You can add any UI inside Loading, including a Skeleton.
+    return <LoadingSkeleton />
   }
